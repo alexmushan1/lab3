edfe20abc1ddbdf6d2f8055645d56cf9799aaac9
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2022-10-13 11:00:49 -0700

    ArrayExamples fixed

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,12 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  
+  @Test
+  public void testReversedWithNum(){
+    int[] input ={1,2,3,4};
+    int[] output ={4,3,2,1};
+    assertArrayEquals(output,ArrayExamples.reversed(input));
+    
+  }
 }
